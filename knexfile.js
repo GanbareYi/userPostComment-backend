54881c9244a4992e34a77121267546767d32cce4
Yi Wu <125087591+GanbareYi@users.noreply.github.com>
2024-01-06 23:26:21 -0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: postgres://hlmzcqbt:H_9ZR7JmiigigDoqZgpnJsVsXNnnJ10R@mahmud.db.elephantsql.com/hlmzcqbt",
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
